f430b9379963a4d774ff3b3b96bf5be1b0885e40
Anzemator <48290702+Anzemator@users.noreply.github.com>
2019-12-26 16:42:36 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$('.team__slider').slick({
+   infinite: true,
+    arrows: false,
+   dots: true,
+   dotsClass: "dots-style",
+    focusOnSelect: true
+});  
+$('.vertical-slider').slick({
+   infinite: true,
+    arrows: false,
+    vertical: true,
+    verticalSwiping: true,
+   dots: true,
+    respondTo: "min",
+   dotsClass: "dots-news",
+    mobileFirst: true,
+}); 
