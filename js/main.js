9b07766d14cdcd0a27b14e96f30e4310a1d70c3b
Anzemator <48290702+Anzemator@users.noreply.github.com>
2019-12-27 13:17:26 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+$(document).ready(function() {
+ $('.work__btn[filter]').click(function(){
+    if($(this).attr('filter')=='all'){
+        if($(this).attr('val')=='off'){
+            $('.work__btn[filter]').attr('val', 'off');
+            $(this).attr('val', 'on');
+            $('.works__cards').show(300);
+        }
+    }else 
+    if($(this).attr('val')=='off'){
+      $('.work__btn[filter]').attr('val', 'off');
+      $(this).attr('val', 'on');
+      $('.works__cards').hide(300);
+      var filter = $(this).attr('filter');
+      $('.works__cards[filter='+filter+']').show(300);
+    }
+ })
+});
 $('.team__slider').slick({
    infinite: true,
     arrows: false,
@@ -15,3 +33,4 @@ $('.vertical-slider').slick({
    dotsClass: "dots-news",
     mobileFirst: true,
 }); 
+
